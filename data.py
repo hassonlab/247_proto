8b927adc799f410ef2622c1c090e82f393342b7a
Harsha <hvgazula@umich.edu>
2023-12-21 11:41:40 -0500

    removed tfs_constants.py

--- data.py
+++ data.py
@@ -8,8 +8,63 @@ from typing import Any, Dict, List, Optional
 from absl import app, flags
 
 import data_pb2
-import tfs_constants
-from tfs_constants import DATA_DIR_MAP, DATUM_FILE_MAP
+
+DATA_DIR = "/projects/HASSON/247/data"
+PODCAST_DATA_DIR = DATA_DIR + "/podcast-data"
+TFS_DATA_DIR = DATA_DIR + "/conversations-car"
+
+DATA_DIR_MAP = {
+    "podcast": PODCAST_DATA_DIR,
+    "tfs": TFS_DATA_DIR,
+}
+
+EXCLUDE_WORDS = ["sp", "{lg}", "{ns}", "{LG}", "{NS}", "SP"]
+
+NON_WORDS = ["hm", "huh", "mhm", "mm", "oh", "uh", "uhuh", "um"]
+
+SUBJECTS = {
+    "podcast": [
+        "661",
+        "662",
+        "717",
+        "723",
+        "737",
+        "741",
+        "742",
+        "743",
+        "763",
+        "798",
+    ],
+    "tfs": ["625", "676", "7170", "798", "7986"],
+}
+
+ELECTRODE_FOLDER_MAP = {
+    "podcast": dict.fromkeys(
+        SUBJECTS["podcast"],
+        "preprocessed_all",
+    ),
+    "tfs": dict.fromkeys(["625", "676"], "preprocessed")
+    | dict.fromkeys(["7170"], "preprocessed_v2")
+    | dict.fromkeys(["798"], "preprocessed_allElec"),
+}
+
+DATUM_FILE_MAP = {
+    "podcast": dict.fromkeys(
+        SUBJECTS["podcast"],
+        "*trimmed.txt",
+    ),
+    "tfs": dict.fromkeys(["625", "676"], "*trimmed.txt")
+    | dict.fromkeys(["7170", "798"], "*_datum_trimmed.txt"),
+}
+
+CONVERSATIONS_MAP = {
+    "podcast": dict.fromkeys(
+        SUBJECTS["podcast"],
+        1,
+    ),
+    "tfs": dict.fromkeys(["625", "676", "7170", "798"], [54, 78, 24, 15]),
+}
+
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("project", None, "Project ID")
@@ -184,7 +239,7 @@ def get_electrode_folder(project: str, data_dir: str, conversation: str) -> str:
     Returns:
         str: The path to the electrode folder.
     """
-    electrode_folder = tfs_constants.ELECTRODE_FOLDER_MAP[project][
+    electrode_folder = ELECTRODE_FOLDER_MAP[project][
         os.path.basename(data_dir)
     ]
     return os.path.join(data_dir, conversation, electrode_folder)
@@ -326,7 +381,7 @@ def main(_):
     project = FLAGS.project
     subject = FLAGS.subject
 
-    if project not in tfs_constants.DATA_DIR_MAP:
+    if project not in DATA_DIR_MAP:
         raise ValueError("Unknown project: {}".format(FLAGS.project))
 
     data = data_pb2.Data()
