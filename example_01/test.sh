536a45291e914b068a9636c9a8e85570f81bc054
Harsha <hvgazula@umich.edu>
2023-12-24 09:08:35 -0500

    some renaming in example_01

--- example_01/test.sh
+++ example_01/test.sh
@@ -1,5 +1,4 @@
-python data.py --project tfs --subject 625 --data_dir /projects/HASSON/247/data/conversations-car
-python data.py --project tfs --subject 676 --data_dir /projects/HASSON/247/data/conversations-car
+protocnew -I=. --python_out=. data.proto
 
-python data.py --project podcast --subject 661 --data_dir /projects/HASSON/247/data/podcast-data
-python data.py --project podcast --subject 717 --data_dir /projects/HASSON/247/data/podcast-data
+python add_patient.py --project tfs --subject 625 --data_dir /projects/HASSON/247/data/conversations-car
+python add_patient.py --project podcast --subject 717 --data_dir /projects/HASSON/247/data/podcast-data
